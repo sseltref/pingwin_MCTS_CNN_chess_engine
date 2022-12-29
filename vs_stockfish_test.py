92e0cb6d2133bf04259ecbce025c80227fd69556
sseltref <szymonmilosz99@gmail.com>
2022-12-29 18:50:02 +0100

    stockfish test

--- vs_stockfish_test.py
+++ vs_stockfish_test.py
@@ -24,7 +24,7 @@ time = 600
 chess_game = pingwin_game(exploration_constant=float(settings['exploration_constant']),bias_constant=float(settings['bias_constant']),n_init=int(settings['init_node_visits']),initboard=board,killer_rate=float(settings['killer_rate']), time_limit=time, simulation_depth=int(settings['simulation_depth']), print_limit=int(settings['moves_print_limit']), show_variation=settings['show_best_line'])
 while True:
     gc.collect()
-    chess_game.make_move(120)
+    chess_game.make_move(60)
     board_fen = chess_game.board.fen()
     stockfish.set_fen_position(board_fen)
     stockfish_move = str(stockfish.get_best_move())
